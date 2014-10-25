3a034535c0dadc377a010f579272ebdcf0ddae00
Eero af heurlin <eero.afheurlin@iki.fi>
2014-10-26 02:30:19 +0300

    check the tag

--- go/test_goroutines.go
+++ go/test_goroutines.go
@@ -4,7 +4,7 @@ import (
     "fmt"
     "github.com/fuzxxl/nfc/2.0/nfc"    
     "github.com/fuzxxl/freefare/0.3/freefare"    
-//    "code.google.com/p/go-sqlite/go1/sqlite3"
+    "code.google.com/p/go-sqlite/go1/sqlite3"
     "time"
 )
 
@@ -15,11 +15,18 @@ func heartbeat() {
     }
 }
 
+
 func main() {
     d, err := nfc.Open("");
     if err != nil {
         panic(err);
     }
+
+    c, err := sqlite3.Open("keys.db")
+    if err != nil {
+        panic(err);
+    }
+    row := make(sqlite3.RowMap)
 
     go heartbeat()
     // mainlopp
@@ -37,12 +44,26 @@ func main() {
             time.Sleep(100 * time.Millisecond)
             //fmt.Println("...polling")
         }
+        valid_found := false
         for i := 0; i < len(tags); i++ {
             tag := tags[i]
             uidstr := tag.UID()
             fmt.Println("Found tag", uidstr);
-            // TODO: Check tag
-            // TODO: do not rediscover same tag at every iteration (ie track state somehow)
+            sql := "SELECT rowid, * FROM keys where uid=?"
+            for s, err := c.Query(sql, uidstr); err == nil; err = s.Next() {
+                var rowid int64
+                s.Scan(&rowid, row)     // Assigns 1st column to rowid, the rest to row
+                fmt.Println(rowid, row)
+                valid_found = true
+            }
         }
+        if valid_found {
+            // TODO: run relay in subroutine
+            fmt.Println("Access GRANTED")
+        } else {
+            fmt.Println("Access DENIED")
+        }
+        // Wait a moment before continuing with fast polling
+        time.Sleep(500 * time.Millisecond)
     }
 }
