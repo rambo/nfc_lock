699ed61385fd19084e0135a7c936c1a14d18c4b6
Eero af Heurlin <rambo@iki.fi>
2014-11-08 21:48:06 +0000

    update the YAML example

--- go/test_yaml.go
+++ go/test_yaml.go
@@ -13,17 +13,30 @@ func main() {
         panic(err);
     }
 //    fmt.Println(dat);
-    
     m := make(map[interface{}]interface{});
     err = yaml.Unmarshal([]byte(dat), &m);
     fmt.Println(m);
+
+    key := m["card_master"].(string)
+    fmt.Println(key);
+    keybytes, err := hex.DecodeString(key);
+    fmt.Println(keybytes);
+
+    dat2, err := ioutil.ReadFile("apps.yaml");
+    if err != nil {
+        panic(err);
+    }
+    m2 := make(map[interface{}]interface{});
+    err = yaml.Unmarshal([]byte(dat2), &m2);
+    fmt.Println(m2);
+    
     
 //    foo := m["aclapp"]
 //    fmt.Println(foo);
     // ugh....
-    key := m["aclapp"].(map[interface{}]interface{})["read"].(string)
-    fmt.Println(key);
+    aid := m2["hacklab_acl"].(map[interface{}]interface{})["aid"].(string)
+    fmt.Println(aid);
+    aidbytes, err := hex.DecodeString(aid);
+    fmt.Println(aidbytes);
 
-    keybytes, err := hex.DecodeString(key);
-    fmt.Println(keybytes);
 }
