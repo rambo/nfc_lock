82b9f3d5c9c8d5a6ecc9ab3ac3145a95643badda
Eero af Heurlin <rambo@iki.fi>
2014-11-09 00:08:33 +0000

    UNTESTED: setting application keys

--- go/personalize.go
+++ go/personalize.go
@@ -8,7 +8,7 @@ import (
     "encoding/binary"
     "github.com/fuzxxl/nfc/2.0/nfc"    
     "github.com/fuzxxl/freefare/0.3/freefare"
-//    "./keydiversification"
+    "./keydiversification"
 )
 
 // TODO: move to a separate helper module
@@ -22,6 +22,21 @@ func string_to_aeskey(keydata_str string) (*freefare.DESFireKey, error) {
     copy(keydata[0:], to_keydata)
     key := freefare.NewDESFireAESKey(*keydata, 0)
     return key,nil
+}
+
+func bytes_to_aeskey(source []byte) (*freefare.DESFireKey) {
+    keydata := new([16]byte)
+    copy(keydata[0:], source)
+    key := freefare.NewDESFireAESKey(*keydata, 0)
+    return key
+}
+
+func string_to_byte(source string) (byte, error) {
+    bytearray, err := hex.DecodeString(source)
+    if err != nil {
+        return 0x0, err
+    }
+    return bytearray[0], nil
 }
 
 func main() {
@@ -47,6 +62,7 @@ func main() {
         panic(err)
     }
 
+    // Application-id from config
     aidbytes, err := hex.DecodeString(appmap["hacklab_acl"].(map[interface{}]interface{})["aid"].(string))
     if err != nil {
         panic(err)
@@ -57,22 +73,49 @@ func main() {
     }
     aid := freefare.NewDESFireAid(uint32(aidint))
     fmt.Println(aid)
+    // Needed for diversification
+    sysid, err := hex.DecodeString(appmap["hacklab_acl"].(map[interface{}]interface{})["sysid"].(string))
+    if err != nil {
+        panic(err)
+    }
 
+    // Key id numbers from config
+    uid_read_key_id, err := string_to_byte(appmap["hacklab_acl"].(map[interface{}]interface{})["uid_read_key_id"].(string))
+    if err != nil {
+        panic(err)
+    }
+    acl_read_key_id, err := string_to_byte(appmap["hacklab_acl"].(map[interface{}]interface{})["acl_read_key_id"].(string))
+    if err != nil {
+        panic(err)
+    }
+    acl_write_key_id, err := string_to_byte(appmap["hacklab_acl"].(map[interface{}]interface{})["acl_write_key_id"].(string))
+    if err != nil {
+        panic(err)
+    }
+    prov_key_id, err := string_to_byte(appmap["hacklab_acl"].(map[interface{}]interface{})["provisioning_key_id"].(string))
+    if err != nil {
+        panic(err)
+    }
+
+    // Defaul (null) key
     nullkeydata := new([8]byte)
     defaultkey := freefare.NewDESFireDESKey(*nullkeydata)
 
+    // New card master key
     new_master_key, err := string_to_aeskey(keymap["card_master"].(string))
     if err != nil {
         panic(err)
     }
     fmt.Println(new_master_key)
 
+    // The static app key to read UID
     uid_read_key, err := string_to_aeskey(keymap["uid_read_key"].(string))
     if err != nil {
         panic(err)
     }
     fmt.Println(uid_read_key)
-    
+
+    // Bases for the diversified keys    
     prov_key_base, err := hex.DecodeString(keymap["prov_master"].(string))
     if err != nil {
         panic(err)
@@ -87,7 +130,7 @@ func main() {
     }
 
 
-
+    // Open device and get tags list
     d, err := nfc.Open("");
     if err != nil {
         panic(err)
@@ -98,6 +141,7 @@ func main() {
         panic(err)
     }
 
+    // Initialize each tag with our app
     for i := 0; i < len(tags); i++ {
         tag := tags[i]
 		fmt.Println(tag.String(), tag.UID())
@@ -129,6 +173,33 @@ func main() {
 		}
         fmt.Println("Done");
 
+        // Get card real UID        
+        realuid_str, error := desfiretag.CardUID()
+        if error != nil {
+            panic(error)
+        }
+        realuid, error := hex.DecodeString(realuid_str);
+        if error != nil {
+            panic(error)
+        }
+
+        // Calculate the diversified keys
+        prov_key_bytes, err := keydiversification.AES128(prov_key_base, aidbytes, realuid, sysid)
+        if err != nil {
+            panic(err)
+        }
+        prov_key := bytes_to_aeskey(prov_key_bytes)
+        acl_read_bytes, err := keydiversification.AES128(acl_read_base, aidbytes, realuid, sysid)
+        if err != nil {
+            panic(err)
+        }
+        acl_read_key := bytes_to_aeskey(acl_read_bytes)
+        acl_write_bytes, err := keydiversification.AES128(acl_write_base, aidbytes, realuid, sysid)
+        if err != nil {
+            panic(err)
+        }
+        acl_write_key := bytes_to_aeskey(acl_write_bytes)
+
 
         fmt.Println("Changing default key");
         error = desfiretag.ChangeKey(0, *new_master_key, *defaultkey);
@@ -155,8 +226,30 @@ func main() {
 		}
         fmt.Println("Done");
 
-        fmt.Println("Changing static auth key");
-        error = desfiretag.ChangeKey(0, *new_master_key, *defaultkey);
+        fmt.Println("Changing static UID reading key");
+        error = desfiretag.ChangeKey(uid_read_key_id, *uid_read_key, *defaultkey);
+		if error != nil {
+			panic(error)
+		}
+        fmt.Println("Done");
+
+
+        fmt.Println("Changing ACL reading key");
+        error = desfiretag.ChangeKey(acl_read_key_id, *acl_read_key, *defaultkey);
+		if error != nil {
+			panic(error)
+		}
+        fmt.Println("Done");
+
+        fmt.Println("Changing ACL writing key");
+        error = desfiretag.ChangeKey(acl_write_key_id, *acl_write_key, *defaultkey);
+		if error != nil {
+			panic(error)
+		}
+        fmt.Println("Done");
+
+        fmt.Println("Changing provisioning key");
+        error = desfiretag.ChangeKey(prov_key_id, *prov_key, *defaultkey);
 		if error != nil {
 			panic(error)
 		}
