b002802b97d5da13f5ee067a153eccbfd82470c8
Eero af heurlin <eero.afheurlin@iki.fi>
2014-11-15 23:23:18 +0200

    scan for devices if none is given on cli, something in the scan leaks memory btw...
    Using device pn532_uart:/dev/ttyUSB0
    ==7746==
    ==7746== HEAP SUMMARY:
    ==7746==     in use at exit: 1,736 bytes in 5 blocks
    ==7746==   total heap usage: 516 allocs, 511 frees, 49,427 bytes
    allocated
    ==7746==
    ==7746== Searching for pointers to 5 not-freed blocks
    ==7746== Checked 169,904 bytes
    ==7746==
    ==7746== LEAK SUMMARY:
    ==7746==    definitely lost: 0 bytes in 0 blocks
    ==7746==    indirectly lost: 0 bytes in 0 blocks
    ==7746==      possibly lost: 0 bytes in 0 blocks
    ==7746==    still reachable: 1,736 bytes in 5 blocks
    ==7746==         suppressed: 0 bytes in 0 blocks
    ==7746== Reachable blocks (those to which a pointer was found) are not
    shown.
    ==7746== To see them, rerun with: --leak-check=full
    --show-leak-kinds=all
    ==7746==
    ==7746== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
    ==7746== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)

--- c/read_personalized.c
+++ c/read_personalized.c
@@ -18,12 +18,7 @@
 
 int main(int argc, char *argv[])
 {
-    // Connect to first by default, or to the connstring specified on CLI
-    nfc_connstring connstring;
-    if (argc == 2)
-    {
-        strncpy(connstring, argv[1], NFC_BUFSIZE_CONNSTRING);
-    }
+    int error = EXIT_SUCCESS;
 
     nfc_context *nfc_ctx;
     nfc_init (&nfc_ctx);
@@ -32,12 +27,48 @@ int main(int argc, char *argv[])
         errx(EXIT_FAILURE, "Unable to init libnfc (propbably malloc)");
     }
 
+
+    // Connect to first by default, or to the connstring specified on CLI
+    nfc_connstring connstring;
     nfc_device *device = NULL;
-    device = nfc_open (nfc_ctx, connstring);
-    if (!device)
+    if (argc == 2)
     {
-        errx(EXIT_FAILURE, "Unable to open device");
+        strncpy(connstring, argv[1], NFC_BUFSIZE_CONNSTRING);
+        device = nfc_open (nfc_ctx, connstring);
+        if (!device)
+        {
+            errx(EXIT_FAILURE, "Unable to open device %s", argv[1]);
+        }
     }
+    else
+    {
+        nfc_connstring devices[8];
+        size_t device_count;
+        device_count = nfc_list_devices(nfc_ctx, devices, 8);
+        if (device_count <= 0)
+        {
+            errx (EXIT_FAILURE, "No NFC device found.");
+        }
+        for (size_t d = 0; d < device_count; ++d)
+        {
+            device = nfc_open (nfc_ctx, devices[d]);
+            if (!device)
+            {
+                warn("nfc_open() failed for %s", devices[d]);
+                error = EXIT_FAILURE;
+                continue;
+            }
+            strncpy(connstring, devices[d], NFC_BUFSIZE_CONNSTRING);
+            break;
+        }
+        if (error != EXIT_SUCCESS)
+        {
+            errx(error, "Could not open any device");
+        }
+    }
+    printf("Using device %s\n", connstring);
+
+
 
     
     nfc_exit(nfc_ctx);
