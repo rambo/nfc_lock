939a0b14f45818fc859e95fdbf5da467621ec9cb
Eero af heurlin <eero.afheurlin@iki.fi>
2014-11-15 21:28:31 +0200

    need to pass the size correctly to the comparison function too

--- c/keydiversification_test.c
+++ c/keydiversification_test.c
@@ -7,12 +7,8 @@
 
 #include "keydiversification.h"
 
-uint8_t expect_key[16] = { 0xA8, 0xDD, 0x63, 0xA3, 0xB8, 0x9D, 0x54, 0xB3, 0x7C, 0xA8, 0x02, 0x47, 0x3F, 0xDA, 0x91, 0x75 };
-
-bool keys_equal(uint8_t *key_A, uint8_t *key_B)
+bool keys_equal(uint8_t *key_A, size_t size_A, uint8_t *key_B, size_t size_B)
 {
-    size_t size_A = sizeof(key_A);
-    size_t size_B = sizeof(key_B);
     if (size_A != size_B)
     {
         return false;
@@ -27,7 +23,6 @@ bool keys_equal(uint8_t *key_A, uint8_t *key_B)
     return true;
 }
 
-// For some reason sizeof returns something completely wonky when trying to get the size of the array behind the pointer
 void pretty_print_key(uint8_t *tag, size_t size) {
     size_t index;
     for (index = 0; index < size - 1; ++index)
@@ -43,7 +38,8 @@ void pretty_print_key(uint8_t *tag, size_t size) {
 
 int main() {
     // These could be constants too but emulate normal usage...
-    uint8_t new_key[16]; // Don't bother including libcrypto just for the block size
+    uint8_t new_key[16]; // Don't bother including libcrypto just for the key size constant
+    uint8_t expect_key[16] = { 0xA8, 0xDD, 0x63, 0xA3, 0xB8, 0x9D, 0x54, 0xB3, 0x7C, 0xA8, 0x02, 0x47, 0x3F, 0xDA, 0x91, 0x75 };
     uint8_t base_key[16] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
     uint8_t aid[3] = { 0x30, 0x42, 0xF5 };
     uint8_t sysid[] = { 0x4E, 0x58, 0x50, 0x20, 0x41, 0x62, 0x75 };
@@ -62,7 +58,7 @@ int main() {
     printf("got:      ");
     pretty_print_key(new_key, sizeof(new_key));
     
-    if (!keys_equal(new_key, expect_key))
+    if (!keys_equal(new_key, sizeof(new_key), expect_key, sizeof(expect_key)))
     {
         printf("ERROR: new_key does not match expected key\n");
         return 1;
