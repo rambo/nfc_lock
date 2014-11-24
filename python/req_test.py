f945e4d8bbd160fe67b51aa96ecb88ef955c5c99
Eero af heurlin <eero.afheurlin@iki.fi>
2014-11-24 21:06:20 +0200

    loop and use correct receive method

--- python/req_test.py
+++ python/req_test.py
@@ -4,6 +4,8 @@ from __future__ import with_statement
 import os, sys
 import yaml
 import zmq
+import time
+from zmq.eventloop.zmqstream import ZMQStream
 
 
 if __name__ == '__main__':
@@ -18,11 +20,11 @@ if __name__ == '__main__':
     socket = context.socket(zmq.REQ)
     socket.connect(config['rep_socket'].replace('tcp://*:','tcp://127.0.0.1:'))
     
-    print("Sending request")
-    socket.send("Hello")
-    print("Waiting for reply")
-    message = socket.recv()
-    print("Got reply: %s" % repr(message))
-    
+    while(True):
+        print("Sending request")
+        socket.send("Hello")
+        print("Waiting for reply")
+        message = socket.recv_multipart()
+        print("Got reply: %s" % repr(message))
+        time.sleep(1)
 
-    
