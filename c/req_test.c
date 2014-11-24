e91e2483d432c6941c6b322a5ab05ec1cc877319
Eero af heurlin <eero.afheurlin@iki.fi>
2014-11-24 23:20:45 +0200

    remove the incorrect example code I copied for reference, this now works for receiving N part messages

--- c/req_test.c
+++ c/req_test.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #define ZMQ_NUM_IOTHREADS 1
 
@@ -20,7 +22,7 @@ int main(int argc, char *argv[])
     err = zmq_connect(requester, argv[1]);
     if (err != 0)
     {
-        printf("ERROR: zmq_connect failed with %d", zmq_errno());
+        printf("ERROR: zmq_connect failed with %s\n", zmq_strerror(zmq_errno()));
         goto END;
     }
 
@@ -29,68 +31,61 @@ int main(int argc, char *argv[])
     err = zmq_msg_init_size(&request, uidlen);
     if (err != 0)
     {
-        printf("ERROR: zmq_msg_init_size failed with %d", zmq_errno());
+        printf("ERROR: zmq_msg_init_size failed with %s\n", zmq_strerror(zmq_errno()));
         goto END;
     }
     memcpy(zmq_msg_data(&request), argv[2], uidlen);
-    printf("Sending request");
-    zmq_send(&request, requester, 0);
+    printf("Sending request\n");
+    err = zmq_send(requester, &request, 0);
+    if (err != 0)
+    {
+        printf("ERROR: zmq_send failed with %s\n", zmq_strerror(zmq_errno()));
+        goto END;
+    }
     zmq_msg_close(&request);
 
-    printf("Waiting for response");
-    int partno;
+    printf("Waiting for response\n");
+    int partno=0;
     while (1)
     {
         partno++;
         zmq_msg_t message;
         zmq_msg_init(&message);
-        zmq_recv(requester, &message, 0);
+        if (err != 0)
+        {
+            printf("ERROR: zmq_msg_init failed with %s\n", zmq_strerror(zmq_errno()));
+            goto END;
+        }
+        err = zmq_recv(requester, &message, 0);
+        if (err != 0)
+        {
+            zmq_msg_close (&message);
+            printf("ERROR: zmq_recv failed with %s\n", zmq_strerror(zmq_errno()));
+            goto END;
+        }
 
-        printf("Received part %d, %d bytes", partno, message.size);
+        printf("Received part %d, %d bytes\n", partno, (int)zmq_msg_size(&message));
+        // TODO: read the part data
 
+        // Done with message
         zmq_msg_close (&message);
-        int more;
+        
+        // See if we have more parts
+        int64_t more;
         size_t more_size = sizeof(more);
-        zmq_getsockopt (requester, ZMQ_RCVMORE, &more, &more_size);
+        err = zmq_getsockopt(requester, ZMQ_RCVMORE, &more, &more_size);
+        if (err != 0)
+        {
+            printf("ERROR: zmq_getsockopt failed with %s\n", zmq_strerror(zmq_errno()));
+            goto END;
+        }
         if (!more)
         {
             break;
         }
     }
-    printf("All %d parts received", partno);
+    printf("All %d parts received\n", partno);
 
-    
-/*
-int main (void)
-{
-    void *context = zmq_ctx_new ();
-
-    //  Socket to talk to server
-    printf ("Connecting to hello world server…\n");
-    void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:5555");
-
-    int request_nbr;
-    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq_msg_t request;
-        zmq_msg_init_size (&request, 5);
-        memcpy (zmq_msg_data (&request), "Hello", 5);
-        printf ("Sending Hello %d…\n", request_nbr);
-        zmq_msg_send (&request, requester, 0);
-        zmq_msg_close (&request);
-
-        zmq_msg_t reply;
-        zmq_msg_init (&reply);
-        zmq_msg_recv (&reply, requester, 0);
-        printf ("Received World %d\n", request_nbr);
-        zmq_msg_close (&reply);
-    }
-    zmq_close (requester);
-    zmq_ctx_destroy (context);
-    return 0;
-}
-
-*/
 END:
     zmq_close(requester);
     zmq_term(context);
