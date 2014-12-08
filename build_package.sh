8fc056c42607075a8818a86283f3b30ac5124f56
Eero af heurlin <eero.afheurlin@iki.fi>
2014-12-08 22:47:11 +0200

    script to make a tarball you can uncompress on your target node so it does not have to contain the build dependencies

---
+++ build_package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+if [ -e build ]
+then
+    rm -rf build
+fi
+set -e
+mkdir -p build/etc/init
+mkdir -p build/opt/nfc_lock/bin
+cd c
+make gatekeeper
+cd ..
+cp -aux c/gatekeeper build/opt/nfc_lock/bin/
+cp -aux python/keyserver.py build/opt/nfc_lock/bin/
+cp -aux python/card_watcher.py build/opt/nfc_lock/bin/
+cp -aux python/usergpio.py build/opt/nfc_lock/bin/
+cp -aux bash/sync_keydb.sh build/opt/nfc_lock/bin/
+cp -aux upstart/*.conf build/etc/init/
+cd build
+tar -cvzf ../nfc_lock-`uname -m`-`date +%Y%m%d_%H%M`.tar.gz ./
+rm -rf build
