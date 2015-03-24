220c7bb171047465c97d7819e653a766b7a4b556
Eero af heurlin <eero.afheurlin@iki.fi>
2015-03-24 21:34:10 +0200

    use the log helpers for messages

--- c/helpers.c
+++ c/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include "log.h"
 
 uint8_t nfclock_applicationsettings(uint8_t accesskey, bool frozen, bool req_auth_fileops, bool req_auth_dir, bool allow_master_key_chg)
 {
@@ -41,6 +42,7 @@ int nfclock_write_uint32(MifareTag tag, uint8_t fileid, uint32_t data)
     if (wrote < 4)
     {
         // TODO: how to raise a sane error
+        log_error("Writing value failed, %s", freefare_strerror(tag));
         return -1;
     }
     return 0;
@@ -56,10 +58,11 @@ int nfclock_read_uint32(MifareTag tag, uint8_t fileid, uint32_t *target)
     if (read < 4)
     {
         // TODO: how to raise a sane error
+        log_error("Reading value failed, %s", freefare_strerror(tag));
         return -1;
     }
     tmpdata = (databytes[0] | (databytes[1] << 8) | (databytes[2] << 16) | (databytes[3] << 24));
-    printf("nfclock_read_uint32: got 0x%lx \n", (unsigned long)tmpdata);
+    log_debug("nfclock_read_uint32: got 0x%lx \n", (unsigned long)tmpdata);
     // This triggers *** stack smashing detected ***
     memcpy(target, &tmpdata, sizeof(tmpdata));
     /**
--- c/smart_node_helpers.c
+++ c/smart_node_helpers.c
@@ -1,4 +1,5 @@
 #include "smart_node_helpers.h"
+#include "log.h"
 
 int nfclock_overwrite_acl(MifareTag tag, char *realuid_str, uint32_t newacl)
 {
@@ -9,30 +10,32 @@ int nfclock_overwrite_acl(MifareTag tag, char *realuid_str, uint32_t newacl)
     err = nfclock_diversify_key_aes128((uint8_t *)nfclock_acl_write_key_base, (uint8_t*)nfclock_aid, realuid_str, (uint8_t*)nfclock_sysid, sizeof(nfclock_sysid), diversified_key_data);
     if (err != 0)
     {
-        printf("Can't calculate diversified ACL write key, failing\n");
+        log_error("Can't calculate diversified ACL write key, failing");
         return err;
     }
 
-    printf("Re-Authenticating (ACL write key), ");
+    log_debug("Re-Authenticating (ACL write key) ... ");
     key = mifare_desfire_aes_key_new_with_version((uint8_t*)&diversified_key_data, 0x0);
     err = mifare_desfire_authenticate(tag, nfclock_acl_write_keyid, key);
     if (err < 0)
     {
+        log_error("Re-Authenticating (ACL write key) failed, err %d (%s)", err, freefare_strerror(tag));
         free(key);
         key = NULL;
         return err;
     }
     free(key);
     key = NULL;
-    printf("done\n");
+    log_debug("Re-Auth done");
 
-    printf("Writing ACL value (0x%lx), ", (unsigned long)newacl);
+    log_info("Writing ACL value (0x%lx) ... ", (unsigned long)newacl);
     err = nfclock_write_uint32(tag, nfclock_acl_file_id, newacl);
     if (err < 0)
     {
+        log_error("Writing ACL value failed, err %d (%s)", err, freefare_strerror(tag));
         return err;
     }
-    printf("done\n");
+    log_info("Writing done");
     return 0;
 }
 
