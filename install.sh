c782c9ce4b1016fb81812bb003557d8c6e1c5d88
Mikko Syrjalahti <mikko.syrjalahti@gmail.com>
2020-05-28 17:05:54 +0000

    Change cp to install. Added install of sync_keydb.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TARGET=/opt/nfc_lock
+NFCUSER=hacklab
 
 echo "Stop services.."
 service gatekeeper stop
@@ -8,12 +9,13 @@ service keyserver stop
 service card_watcher stop
 
 echo "Install files.."
-mkdir -p $TARGET/bin
-cp python/keyserver.py $TARGET/bin
-cp python/usergpio.py $TARGET/bin
-cp python/card_watcher.py $TARGET/bin
-cp c/gatekeeper $TARGET/bin
-cp python/config.yml $TARGET/
+install -dm 700 -o $NFCUSER $TARGET/bin
+install -m 700 -o $NFCUSER python/keyserver.py $TARGET/bin
+install -m 600 -o $NFCUSER python/usergpio.py $TARGET/bin
+install -m 700 -o $NFCUSER python/card_watcher.py $TARGET/bin
+install -m 700 -o $NFCUSER c/gatekeeper $TARGET/bin
+install -m 600 -o $NFCUSER python/config.yml $TARGET/
+install -m 700 -o $NFCUSER bash/sync_keydb.sh $TARGET/bin
 
 echo "Install systemd scripts.."
 SYSD=/etc/systemd/system/
